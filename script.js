1abfa247aae67f4f1100052b9f5d4f802b0f677f
Vikaskumarrr <143922604+Vikaskumarrr@users.noreply.github.com>
2023-12-20 22:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+        function incrementCounter() {
+            // Get the current counter value
+            var currentCounter = document.getElementById("counter").innerText;
+
+            // Display an alert with the un-incremented value
+            alert("Un-incremented Value: " + currentCounter);
+
+            // Increment the counter value
+            var newCounter = parseInt(currentCounter) + 1;
+
+            // Update the counter display
+            document.getElementById("counter").innerText = newCounter;
+        }
